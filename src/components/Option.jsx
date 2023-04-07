cc9d786b710afd2de30f439ad22760aa0ccb5e1d
Lucas França Marinho <lucasfrancamarinho16@gmail.com>
2023-04-06 22:31:21 -0300

    feat: Category

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -3,14 +3,23 @@ import { QuizContext } from "../context/quiz"
 
 import "./Option.css";
 
-const Option = ({option, selectOption, answer}) => {
-    const [quizState, dispatch] = useContext(QuizContext);
+const Option = ({ option, selectOption, answer, hide }) => {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className={`option ${quizState.answerSelected && option === answer ? 'correct' : ""} ${quizState.answerSelected && option !== answer ? 'wrong' : ""}`} onClick={() => selectOption()}>
-        <p>{option}</p>
+    <div
+      onClick={() => selectOption()}
+      className={`
+      option
+        ${quizState.answerSelected && option === answer ? "correct" : ""} ${
+        quizState.answerSelected && option !== answer ? "wrong" : ""
+      }
+        ${hide ? "hide" : ""}
+        `}
+    >
+      <p>{option}</p>
     </div>
-  )
-}
+  );
+};
 
 export default Option
